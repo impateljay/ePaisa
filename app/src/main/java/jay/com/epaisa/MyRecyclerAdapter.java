7f990d17d6593d9f55d983d69db269f26a7ffd17
JPatel <pateljay0323@gmail.com>
2016-06-01 15:04:57 +0530

    using Glide library

--- app/src/main/java/jay/com/epaisa/MyRecyclerAdapter.java
+++ app/src/main/java/jay/com/epaisa/MyRecyclerAdapter.java
@@ -1,14 +1,25 @@
 package jay.com.epaisa;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 /**
@@ -19,8 +30,8 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<CustomViewHolder> {
     private List<SongItem> songItemList;
     private Context mContext;
 
-    public MyRecyclerAdapter(Context context, List<SongItem> feedItemList) {
-        this.songItemList = feedItemList;
+    public MyRecyclerAdapter(Context context, List<SongItem> songItemList) {
+        this.songItemList = songItemList;
         this.mContext = context;
     }
 
@@ -37,10 +48,18 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<CustomViewHolder> {
         SongItem songItem = songItemList.get(i);
 
         //Download image using picasso library
-        Picasso.with(mContext).load(songItem.getArtworkUrl100())
+//        Picasso.with(mContext).load(songItem.getArtworkUrl100())
+//                .error(R.drawable.placeholder)
+//                .placeholder(R.drawable.placeholder)
+//                .into(customViewHolder.imageView);
+
+        Glide.with(mContext).load(songItem.getArtworkUrl100())
                 .error(R.drawable.placeholder)
                 .placeholder(R.drawable.placeholder)
                 .into(customViewHolder.imageView);
+
+        //new BitmapWorkerTask(customViewHolder.imageView).execute(songItem.getArtworkUrl100());
+        //customViewHolder.imageView.setImageBitmap(new BitmapWorkerTask(customViewHolder.imageView).execute(songItem.getArtworkUrl100()));
 
         //Setting text view title
         customViewHolder.textView.setText(Html.fromHtml(songItem.getTrackName()));
@@ -49,5 +68,65 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<CustomViewHolder> {
     @Override
     public int getItemCount() {
         return (null != songItemList ? songItemList.size() : 0);
+    }
+
+    class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private String imageUrl;
+
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage
+            // collected
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            imageUrl = params[0];
+            return LoadImage(imageUrl);
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (imageViewReference != null && bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+
+        private Bitmap LoadImage(String URL) {
+            Bitmap bitmap = null;
+            InputStream in = null;
+            try {
+                in = OpenHttpConnection(URL);
+                bitmap = BitmapFactory.decodeStream(in);
+                in.close();
+            } catch (IOException e1) {
+            }
+            return bitmap;
+        }
+
+        private InputStream OpenHttpConnection(String strURL)
+                throws IOException {
+            InputStream inputStream = null;
+            URL url = new URL(strURL);
+            URLConnection conn = url.openConnection();
+
+            try {
+                HttpURLConnection httpConn = (HttpURLConnection) conn;
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+
+                if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpConn.getInputStream();
+                }
+            } catch (Exception ex) {
+            }
+            return inputStream;
+        }
     }
 }
